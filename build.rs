100eca425b8aca241755b02a1f87ec80a80447d9
Lain-dono <lain.dono@gmail.com>
2018-03-17 22:12:27 +0300

    rm stub

--- build.rs
+++
@@ -1,6 +0,0 @@
-pub fn main() {
-    println!("cargo:rustc-link-search=native=ext");
-    println!("cargo:rustc-link-lib=static=sd");
-    println!("cargo:rerun-if-changed=ext/layout.ld");
-    println!("cargo:rerun-if-changed=ext/start.S");
-}
