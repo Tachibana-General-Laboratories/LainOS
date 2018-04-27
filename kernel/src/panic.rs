241db6939afca761deecc7c0efcb308b9d31a0b0
Lain-dono <lain.dono@gmail.com>
2018-04-27 03:36:33 +0300

    fix kernel/src/panic.rs

--- kernel/src/panic.rs
+++ kernel/src/panic.rs
@@ -1,21 +1,17 @@
-use console::{kprint, kprintln};
-
-use core::fmt;
+use pi::uart::MiniUart;
+use core::fmt::{self, Write, Arguments};
 
 #[lang = "eh_personality"]
 pub extern "C" fn eh_personality() {}
 
 #[lang = "panic_fmt"]
 #[no_mangle]
-pub extern fn panic_fmt(msg: fmt::Arguments, file: &'static str, line: u32, col: u32) -> ! {
-    kprintln!("\n\n");
-    kprintln!("---------- KERNEL PANIC ----------");
-    kprintln!("");
-    kprintln!("FILE: {}", file);
-    kprintln!("LINE: {}", line);
-    kprintln!(" COL: {}", col);
-    kprintln!("");
-    kprintln!("{}", msg);
+pub extern fn panic_fmt(msg: Arguments, file: &'static str, line: u32, col: u32) -> ! {
+    let mut uart = MiniUart::new();
+
+    let _ = uart.write_str("\n\n---------- KERNEL PANIC ----------\n");
+    let _ = uart.write_fmt(format_args!("FILE: {}\nLINE: {}\n COL: {}\n", file, line, col));
+    let _ = uart.write_fmt(format_args!("{}", msg));
 
     loop { unsafe { asm!("wfe") } }
 }
